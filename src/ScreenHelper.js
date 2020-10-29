6d48a6458ac7d752d99bfd7c230fdc856f90ba5c
Gregor Hollmig <gregor.hollmig@sap.com>
2020-10-29 12:36:21 +0100

    translate to english

--- src/ScreenHelper.js
+++ src/ScreenHelper.js
@@ -6,8 +6,8 @@ class ScreenHelper {
 
   static async askRepoPath(context, options={}) {
     await context.app.alert(
-      'Selecione Repositorio',
-      `Escolha o repositorio para sincronizar workspaces\nWorkspace Atual: ${options.currentPath}`
+      'Select repository',
+      `Chose the repository to synchronize your workspace\nCurrent path: ${options.currentPath}`
     );
     const path = await context.app.showSaveDialog({defaultPath: options.workspaceName});
 
