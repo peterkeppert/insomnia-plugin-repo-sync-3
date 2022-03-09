38db4e7863413d697c9398aed07f2622b9036347
Raphael Bideau <raphael@dotfile.com>
2022-03-09 15:22:02 +0100

    style: add prettier configuration and fix style

--- src/ScreenHelper.js
+++ src/ScreenHelper.js
@@ -1,15 +1,17 @@
 const { dirname } = require('path');
 class ScreenHelper {
   static async alertError(context, message) {
-    return await context.app.alert('Erro!', message);
+    return await context.app.alert('Error!', message);
   }
 
-  static async askRepoPath(context, options={}) {
+  static async askRepoPath(context, options = {}) {
     await context.app.alert(
       'Select repository',
-      `Chose the repository to synchronize your workspace\nCurrent path: ${options.currentPath}`
+      `Choose the repository to synchronize your workspace\nCurrent path: ${options.currentPath}`
     );
-    const path = await context.app.showSaveDialog({defaultPath: options.workspaceName});
+    const path = await context.app.showSaveDialog({
+      defaultPath: options.workspaceName,
+    });
 
     return dirname(path);
   }
